cc7ebff76017114ac74c96a73bd17a6c2e2ca02d
abdusali1234 <abdusali1234@gmail.com>
2024-06-20 10:49:18 +0100

    Created class with method to display tic-tac-toe board

---
+++ lib/board.rb
@@ -0,0 +1,18 @@
+class Board
+    attr_reader :board, :new_board
+
+    def initialize
+        @board = Array.new(9) { |x| x + 1}
+    end
+
+    def display_board
+        puts <<-HEREDOC
+        #{board[0]} | #{board[1]} | #{board[2]}
+        --+---+--
+        #{board[3]} | #{board[4]} | #{board[5]}
+        --+---+--
+        #{board[6]} | #{board[7]} | #{board[8]}
+        HEREDOC
+    end
+
+end
