7cfe6e70ca7a9fa9189448d27d4b5b095b1b6e1e
abdusali1234 <abdusali1234@gmail.com>
2024-06-20 13:09:22 +0100

    Added method to check if a 3-in-a-row is achieved by a player

--- lib/board.rb
+++ lib/board.rb
@@ -20,4 +20,26 @@ class Board
         display_board
     end
 
+    def three_in_a_row?(symbol)
+        winning_combos = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+          ]
+
+        winning_combos.any? do |combo|
+            combo.all? do |pos|
+                board[pos] == symbol
+            end
+        end
+
+
+
+    end
+
 end
