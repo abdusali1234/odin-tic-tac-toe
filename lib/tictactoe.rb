72f48d72aff44ca4533db696588f0c0b0921d974
abdusali1234 <abdusali1234@gmail.com>
2024-06-20 13:52:25 +0100

    Created tictactoe file which contains game functionality

---
+++ lib/tictactoe.rb
@@ -0,0 +1,65 @@
+require_relative 'board'
+require_relative 'player'
+
+class Game
+    attr_reader :game_board, :player_one, :player_two, :moves, :current_player
+
+    def initialize
+        @game_board = Board.new
+        @moves = 1
+    end
+
+    def assign_player_one
+        puts "Player 1, please enter your name: "
+        @player_one = Player(gets.chomp, 'X')
+        @current_player = player_one
+    end
+
+    def assign_player_two
+        puts "Player 2, please enter your name: "
+        @player_one = Player(gets.chomp, 'O')
+    end
+
+
+    def change_current_player
+      current_player == player_one ? player_two : player_one
+    end
+
+    def game_move
+        move = current_player.player_moves
+        move = current_player.player_moves until game_board.can_play_square?(move)
+        move
+    end
+
+    def play_game
+        while @moves <= 9
+          move = game_move
+          game_board.update_board(current_player.symbol, move)
+          if current_player.player_wins?(game_board)
+            current_player.congratulate_winner
+            break
+          end
+          @current_player = change_current_player
+          @moves += 1
+        end
+    end
+
+    def tie_message
+        puts "Tie, no one wins!"
+    end
+
+    def start_game
+        puts "\nLet's Tic-Tac-Toe!\n\n"
+        assign_player_one
+        assign_player_two
+        game_board.display_board
+        play_game
+        tie_message if moves == 10
+    end
+
+
+
+    
+
+
+end
